f7275fbbd6917a2cf356199642ed7af43e6d8f19
ba2704kien2951manh3003 <119926363+ba2704kien2951manh3003@users.noreply.github.com>
2023-01-17 15:04:27 +0700

    Delete syscall.h

--- src/syscall.h
+++
@@ -1,8 +0,0 @@
-#ifndef USERPROG_SYSCALL_H
-#define USERPROG_SYSCALL_H
-
-// System call to exit terminated process
-void syscall_init(void);
-void syscall_exit(int);
-
-#endif /* userprog/syscall.h */
