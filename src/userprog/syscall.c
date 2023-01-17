e6518f5f540cc9e87f1f3c0caf370dd0643eb9a4
ba2704kien2951manh3003 <119926363+ba2704kien2951manh3003@users.noreply.github.com>
2023-01-17 15:08:36 +0700

    upload2

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,19 +3,25 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
-#include "threads/synch.h"
 #include "threads/vaddr.h"
 #include "userprog/pagedir.h"
-#include "threads/init.h"
-#include "devices/shutdown.h" /* Imports shutdown_power_off() for use in halt(). */
-#include "filesys/file.h"
-#include "filesys/filesys.h"
-#include "userprog/process.h"
-#include "devices/input.h"
-#include "threads/malloc.h"
-static void syscall_handler (struct intr_frame *);
 
-/* Add */
+// System call to exit terminated process
+void syscall_init(void);
+void exit(int);
+static void syscall_handler(struct intr_frame *f);
+void get_args(struct intr_frame *f, int argc, int *args);
+bool check_ptr(const void *ptr);
+void syscall_init(void)
+{
+  intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall");
+}
+
+void exit(int status)
+{
+  printf("\n%s: exit(%d)\n\n", thread_current()->name, status);
+  thread_exit();
+}
 bool check_ptr(const void *ptr)
 {
   if (ptr == NULL || !is_user_vaddr(ptr) || pagedir_get_page(thread_current()->pagedir, ptr) == NULL)
@@ -23,29 +29,37 @@ bool check_ptr(const void *ptr)
 
   return true;
 }
-void get_args(struct intr_frame *f, int argc, int *args);
-void halt(void);
-void exit(int status);
-int write (int fd, const void *buffer, unsigned length);
-struct lock filesys_lock;
-/* Creates a struct to insert files and their respective file descriptor into
-   the file_descriptors list for the current thread. */
-struct thread_file
+static void
+syscall_handler(struct intr_frame *f)
 {
-  struct list_elem file_elem;
-  struct file *file_addr;
-  int file_descriptor;
-};
-/* Add */
-void
-syscall_init (void) 
-{
-  /* Add */
-  lock_init(&filesys_lock);
-  /* Add */
-  intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+  int syscall_number;
+  int args[3];
+  ASSERT(sizeof(syscall_number) == 4);
+
+  uint32_t *esp = f->esp;
+  if(!check_ptr(esp))
+    exit(-1);
+  syscall_number = *esp;
+  // The system call number is in the 32-bit word at the caller's stack pointer.
+  // SYS_* constants are defined in syscall-nr.h
+  switch (syscall_number)
+  {
+  case SYS_EXIT:
+  {
+    get_args(f, 1, &args[0]);
+    thread_current()->current_esp = f->esp;
+
+    exit(args[0]);
+    NOT_REACHED();
+    break;
+  }
+  /* unhandled case */
+  default:
+    printf("[ERROR] system call EXIT is unimplemented!\n");
+    exit(-1);
+    break;
+  }
 }
-/* Add */
 void get_args(struct intr_frame *f, int argc, int *args)
 {
   if((argc < 1)||(argc > 3))
@@ -58,87 +72,4 @@ void get_args(struct intr_frame *f, int argc, int *args)
       check_ptr((const void *) ptr);
       args[i] = *ptr;
     }
-    
-}
-/* Add */
-static void
-syscall_handler (struct intr_frame *f UNUSED) 
-{
-  /* Add */
-  int args[3];
-  /* Stores the physical page pointer. */
-  void * phys_page_ptr;
-
-  uint32_t *esp = f->esp;
-  if(!check_ptr(esp))
-    exit(-1);
-  uint32_t syscall_number = *esp;
-  switch(syscall_number)
-  {
-  case SYS_HALT:
-    halt();
-    break;
-  case SYS_EXIT:
-    get_args(f, 1, &args[0]);
-    exit(args[0]);
-    break;
-  case SYS_WRITE:
-    get_args(f, 3, &args[0]);
-    phys_page_ptr = pagedir_get_page(thread_current()->pagedir, (const void *) args[1]);
-    if(phys_page_ptr == NULL)
-    {
-      exit(-1);
-    }
-    args[1] = (int) phys_page_ptr;
-
-    f->eax = write(args[0], (const void*)args[1], (unsigned) args[2]);
-    break;
-  default:
-    exit(-1);
-    break;
-  }
-  /* Add */
-}
-
-/* Add */
-void halt(void)
-{
-  shutdown_power_off();
-}
-void exit(int status)
-{
-  printf("%s: exit(%d)\n", thread_current()->name, status);
-  thread_exit();
-}
-int write (int fd, const void *buffer, unsigned length)
-{
-  // List element to iterate list of file descripor.
-  struct list_elem *temp;
-
-  lock_acquire(&filesys_lock);
-  // df equal to 1 => Write to STDOUT
-  if(fd == 1)
-  {
-    putbuf(buffer, length);
-    lock_release(&filesys_lock);
-    return length;
-  }
-  if(fd == 0 || list_empty(&thread_current()->file_descriptors))
-  {
-    lock_release(&filesys_lock);
-    return 0;
-  }
-  // Iterate file descriptor to find the file to write
-  for (temp = list_front(&thread_current()->file_descriptors); temp != NULL; temp = temp->next)
-  {
-    struct thread_file *t = list_entry(temp, struct thread_file, file_elem);
-    if(t->file_descriptor == fd)
-    {
-      int ret = (int) file_write(t->file_addr, buffer, length);
-      lock_release(&filesys_lock);
-      return ret;
-    }
-  }
-  lock_release(&filesys_lock);
-  return 0;
 }
--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -1,6 +1,8 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
-void syscall_init (void);
+// System call to exit terminated process
+void syscall_init(void);
+void syscall_exit(int);
 
 #endif /* userprog/syscall.h */
